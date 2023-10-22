a54993df30794f6f448de3b630ff9e27e59cc6e4
gaston <gastonvallasciani@gmail.com>
2023-10-22 17:31:31 -0300

    Agrego la versión de firmware

--- src/main.c
+++ src/main.c
@@ -9,9 +9,16 @@
 #include "pwm_manager.h"
 #include "button_manager.h"
 #include "global_manager.h"
+#include "../include/version.h"
+
+static char fw_version[4];
+static uint8_t version_len = 0;
 
 void app_main() 
 {
+    get_version(fw_version, &version_len);
+    printf("FIRMWARE VERSION: %s \n", fw_version);
+
     led_manager_init();
     button_manager_init();
     analog_input_manager_init();
